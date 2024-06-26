d45483f2ded8f24a57f21ab11778c4a33e00e55a
OsmanTunahan <osmantunahan007@gmail.com>
2024-06-27 01:25:05 +0300

    Add Google DNS Service to dns class

--- dns/dns.go
+++ dns/dns.go
@@ -157,3 +157,81 @@ func (g *GenericDNSClient) getCloudflareRecordID(zoneID, domain string) (string,
 
 	return "", errors.New("record ID not found")
 }
+
+func (g *GenericDNSClient) updateGoogleDNS(domain, ip string) error {
+	zoneID, err := g.getGoogleDNSZoneID(domain)
+	if err != nil {
+		return err
+	}
+
+	recordName := domain + "."
+
+	url := fmt.Sprintf("https://dns.googleapis.com/dns/v1/projects/%s/managedZones/%s/rrsets/%s/A", g.projectID, zoneID, recordName)
+	body := map[string]interface{}{
+		"kind":    "dns#resourceRecordSet",
+		"name":    recordName,
+		"type":    "A",
+		"ttl":     300,
+		"rrdatas": []string{ip},
+	}
+	jsonBody, _ := json.Marshal(body)
+
+	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+g.apiKey)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		return fmt.Errorf("failed to update DNS record, status code: %d, response: %s", resp.StatusCode, string(bodyBytes))
+	}
+
+	return nil
+}
+
+func (g *GenericDNSClient) getGoogleDNSZoneID(domain string) (string, error) {
+	url := fmt.Sprintf("https://dns.googleapis.com/dns/v1/projects/%s/managedZones", g.projectID)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+g.apiKey)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to get managed zones, status code: %d", resp.StatusCode)
+	}
+
+	var result map[string]interface{}
+	body, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(body, &result)
+
+	if managedZones, ok := result["managedZones"].([]interface{}); ok {
+		for _, zone := range managedZones {
+			zoneMap := zone.(map[string]interface{})
+			if strings.Contains(domain, zoneMap["dnsName"].(string)) {
+				return zoneMap["id"].(string), nil
+			}
+		}
+	}
+
+	return "", errors.New("managed zone ID not found")
+}
